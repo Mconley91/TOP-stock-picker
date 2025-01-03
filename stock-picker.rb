71806d3eb582a00962927804447a232f6f3d128f
mconley91 <matthew.e.conley@gmail.com>
2025-01-02 21:44:32 -0500

    add gets variable

---
+++ stock-picker.rb
@@ -0,0 +1,8 @@
+puts "Enter an Array of stock prices across a set of days chronologically separated by commas:"
+stock_array = gets.chomp.split(",")
+
+def stock_picker(stock_array)
+  
+end
+
+stock_picker("17,3,6,9,15,8,6,1,10")
